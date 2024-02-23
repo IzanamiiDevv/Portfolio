23301630a72d3081e031620984a8c0a0b8fce994
IzanamiiDevv <rafaeloli596@gmail.com>
2024-02-24 03:52:19 +0800

    sss

--- components/main/Footer.tsx
+++ components/main/Footer.tsx
@@ -58,13 +58,13 @@ const Footer = () => {
                     </p>
                     <p className="flex flex-row items-center my-[15px] cursor-pointer">
                   
-                        <span className="text-[15px] ml-[6px]">mifwebchain@gmail.com</span>    
+                        <span className="text-[15px] ml-[6px]">rafaeloli596@gmail.com</span>
                     </p>
                 </div>
             </div>
 
             <div className="mb-[20px] text-[15px] text-center">
-                &copy; WebChain Dev 2023 Inc. All rights reserved
+                &copy; IzanamiiDevv 2019 Inc. All rights reserved
             </div>
         </div>
     </div>
