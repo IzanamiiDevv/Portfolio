08d5a89da250ca55cb9c61f1feb334c9629461d1
IzanamiiDevv <rafaeloli596@gmail.com>
2024-02-24 01:37:29 +0800

    test

---
+++ app/page.tsx
@@ -0,0 +1,18 @@
+import Encryption from "@/components/main/Encryption";
+import Hero from "@/components/main/Hero";
+import Projects from "@/components/main/Projects";
+import Skills from "@/components/main/Skills";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="h-full w-full">
+      <div className="flex flex-col gap-20">
+        <Hero />
+        <Skills />
+        <Encryption />
+        <Projects />
+      </div>
+    </main>
+  );
+}
