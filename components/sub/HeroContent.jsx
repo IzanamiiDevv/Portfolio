1068cb98475e9e55cec799c14cdd05bec3618444
IzanamiiDevv <rafaeloli596@gmail.com>
2024-02-24 04:08:38 +0800

    ss

--- components/sub/HeroContent.jsx
+++ components/sub/HeroContent.jsx
@@ -24,7 +24,7 @@ const HeroContent = () => {
         >
           <SparklesIcon className="text-[#b49bff] mr-[10px] h-5 w-5" />
           <h1 className="Welcome-text text-[13px]">
-            Fullstack Developer Portfolio
+            Game Developer and FullStack Web Developer
           </h1>
         </motion.div>
 
@@ -33,12 +33,12 @@ const HeroContent = () => {
           className="flex flex-col gap-6 mt-6 text-6xl font-bold text-white max-w-[600px] w-auto h-auto"
         >
           <span>
-            Providing
+            Izanamii
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500">
               {" "}
-              the best{" "}
+              is the best{" "}
             </span>
-            project exprience
+            GameDeveloper
           </span>
         </motion.div>
 
@@ -46,8 +46,8 @@ const HeroContent = () => {
           variants={slideInFromLeft(0.8)}
           className="text-lg text-gray-400 my-5 max-w-[600px]"
         >
-          I&apos;m a Full Stack Software Engineer with experience in Website,
-          Mobile, and Software development. Check out my projects and skills.
+          I&apos;m a Full Stack Web Developer with experience in Programming Since 2019,
+          Im a student at SanFrancisco High School. Check out my projects and skills.
         </motion.p>
         <motion.a
           variants={slideInFromLeft(1)}
