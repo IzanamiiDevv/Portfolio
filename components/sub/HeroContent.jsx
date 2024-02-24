9ae0f67214d23b5452097ee72174a378b06089e6
IzanamiiDevv <rafaeloli596@gmail.com>
2024-02-24 11:57:43 +0800

    ss

--- components/sub/HeroContent.jsx
+++ components/sub/HeroContent.jsx
@@ -15,9 +15,18 @@ const HeroContent = () => {
     <motion.div
       initial="hidden"
       animate="visible"
-      className="flex flex-row items-center justify-center px-20 mt-40 w-full z-[20]"
+      className="flex flex-row items-center justify-center px-20 mt-20 w-full z-[20]"
     >
       <div className="h-full w-full flex flex-col gap-5 justify-center m-auto text-start">
+      <motion.div
+          variants={slideInFromTop}
+          className="Welcome-box py-[8px] px-[7px] border border-[#7042f88b] opacity-[0.9]"
+        >
+          <SparklesIcon className="text-[#b49bff] mr-[10px] h-5 w-5" />
+          <h1 className="Welcome-text text-[13px]">
+            You Can&apos;t Defeat Me At My Own Game.
+          </h1>
+        </motion.div>
         <motion.div
           variants={slideInFromLeft(0.5)}
           className="flex flex-col gap-6 mt-6 text-6xl font-bold text-white max-w-[600px] w-auto h-auto"
@@ -46,15 +55,7 @@ const HeroContent = () => {
         >
           Learn More!
         </motion.a>
-        <motion.div
-          variants={slideInFromTop}
-          className="Welcome-box py-[8px] px-[7px] border border-[#7042f88b] opacity-[0.9]"
-        >
-          <SparklesIcon className="text-[#b49bff] mr-[10px] h-5 w-5" />
-          <h1 className="Welcome-text text-[13px]">
-            You Can&apos;t Defeat Me At My Own Game.
-          </h1>
-        </motion.div>
+        
       </div>
 
       <motion.div
