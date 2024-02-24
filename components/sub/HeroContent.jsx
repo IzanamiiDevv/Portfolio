0f348bdb88278943569de5a7b32aac0082253bad
IzanamiiDevv <rafaeloli596@gmail.com>
2024-02-24 11:51:21 +0800

    aa

--- components/sub/HeroContent.jsx
+++ components/sub/HeroContent.jsx
@@ -17,7 +17,7 @@ const HeroContent = () => {
       animate="visible"
       className="flex flex-row items-center justify-center px-20 mt-40 w-full z-[20]"
     >
-      <div className="h-full w-full flex flex-col gap-5 justify-center m-auto text-start" style="margin-top:60px;">
+      <div className="h-full w-full flex flex-col gap-5 justify-center m-auto text-start">
         <motion.div
           variants={slideInFromLeft(0.5)}
           className="flex flex-col gap-6 mt-6 text-6xl font-bold text-white max-w-[600px] w-auto h-auto"
